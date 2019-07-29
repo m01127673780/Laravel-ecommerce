216a8be4556ce29971132f7365cb146563c39fd0
Mohamed  Elsaeed <m01127673780@gmail.com>
2019-07-29 11:18:28 +0200

    test

--- app/Http/helper.php
+++ app/Http/helper.php
@@ -1,5 +1,11 @@
 <?php
 
+if (!function_exists('setting')) {
+	function setting() {
+		return \App\Model\Setting::orderBy('id', 'desc')->first();
+	}
+}
+
 if (!function_exists('aurl')) {
 	function aurl($url = null) {
 		return url('admin/'.$url);
@@ -9,6 +15,17 @@ if (!function_exists('aurl')) {
 if (!function_exists('admin')) {
 	function admin() {
 		return auth()->guard('admin');
+	}
+}
+
+if (!function_exists('active_menu')) {
+	function active_menu($link) {
+
+		if (preg_match('/'.$link.'/i', Request::segment(2))) {
+			return ['menu-open', 'display:block'];
+		} else {
+			return ['', ''];
+		}
 	}
 }
 
--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -16,14 +16,13 @@
   @if(direction() == 'ltr')
   <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/AdminLTE.min.css">
   @else
+ 
+
 <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/rtl/rtl/AdminLTE-rtl.min.css">
- <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/rtl/rtl/bootstrap-rtl.min.css">
-<link rel="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.1/css/all.min.css">
+<link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/rtl/rtl/bootstrap-rtl.min.css">
+<link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/rtl/rtl/font-awesome-rtl.min.css">
 <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/rtl/rtl/rtl.css">
-<link rel="stylesheet" href="{{ url('/') }}/design/adminlte/jstree/themes/default/style.css">
-<link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/tableRespons.css">
-<script src="{{ url('/design/adminlte/dist/js/myfunctions.js') }}"></script>
-<link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/style-custom.css">  
+
   <link href="https://fonts.googleapis.com/css?family=Cairo:300,400&amp;subset=arabic,latin-ext" rel="stylesheet">
   <style type="text/css">
     html,body ,h1,h2,h3,h4,h5,h6{
@@ -31,12 +30,50 @@
     }
   </style>
   @endif
- 
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
   <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/skins/_all-skins.min.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/bower_components/morris.js/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/bower_components/jvectormap/jquery-jvectormap.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
   <link rel="stylesheet" href="{{ url('/') }}/design/adminlte/jstree/themes/default/style.css">
+
+  
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+
   <script src="{{ url('/design/adminlte/dist/js/myfunctions.js') }}"></script>
+
+
+
+<link rel="stylesheet" href="{{ url('/') }}/design/adminlte/dist/css/style-custom.css">
+
+
+ 
+
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
   <!-- Google Font -->
+  <style type="text/css">
+    
+    .control-sidebar-dark, .control-sidebar-dark+.control-sidebar-bg {
+    background: #222d32;
+    display: none;
+}
+  </style>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
